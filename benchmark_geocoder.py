a9696f61e97ac7289d7d7ac4217cf329ae786185
Hakob Abajian <hakob.abajian@med.usc.edu>
2022-02-25 13:56:20 -0800

    edited benchmark_geocoder

--- benchmark_geocoder.py
+++ benchmark_geocoder.py
@@ -1,19 +1,18 @@
 import os
 import json
+import timeit
 from address import Address
 from geocoder import *
 
 def read_json_file(file):
     with open(file) as address_file:
         address_list = json.load(address_file)['addresses']
-    address_file.close()
     return address_list
 
 def read_json_file_seperate_states(file):
     address_dict = {}
     with open(file) as address_file:
         address_list = json.load(address_file)['addresses']
-    address_file.close()
     for address in address_list:
         state = address['state']
         if state in address_dict.keys():
@@ -50,15 +49,16 @@ def parse_json_dict_seperate_states(address_dict):
     return parsed_dict
 
 
-def geocode_list(parsed_addresses):
-    errors = 0
-    for address in parsed_addresses:
+def geocode_list(parsed_addresses, list_range):
+    start_time = timeit.default_timer()
+    for address in parsed_addresses[:list_range]:
         try:
             geocode_address_to_census_tract(address)
         except(IndexError, KeyError):
-            errors+=1
             pass
-    return errors
+    return (timeit.default_timer() - start_time)/list_range
 
-ad = parse_json_file(read_json_file(os.getcwd()+r'\gis-toolkit\input\addresses-us-all.json'))
+ad = parse_json_file(read_json_file(os.getcwd()+r'\input\addresses-us-all.json'))
+
+print(geocode_list(ad,100))
 
