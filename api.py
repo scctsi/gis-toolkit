4b3711028446d120992d6107b9299dc492ee0fbb
Hakob Abajian <hakob.abajian@med.usc.edu>
2022-03-25 15:33:47 -0700

    pre-merge commit

--- api.py
+++ api.py
@@ -9,7 +9,7 @@ def construct_url(interpolation_string, arguments):
 
 def get_response(url):
     # TODO: Assert 200
-    response = requests.get(url)
+    response = requests.get(url, verify=False)
 
     try:
         return response.json()
@@ -33,6 +33,7 @@ def get_value(url):
     if response == constant.NOT_AVAILABLE:
         return constant.NOT_AVAILABLE
     else:
+        print(response)
         header_row, truncated_response = get_header_row_and_truncated_json(response)
         # TODO: This is specific to the Census API which returns JSON like this example below:
         # [['B19083_001E', 'state', 'county', 'tract'], ['0.4981', '06', '001', '400100']]
--- validate_sedoh_variables.py
+++ validate_sedoh_variables.py
@@ -1,5 +1,6 @@
 import random
 import constant
+from data_frame_enhancer import GeoenhancedCache
 from data_structure import GetStrategy
 import sedoh_data_structure as sds
 import value_getter
@@ -11,30 +12,32 @@ import math
 
 
 def validate_sedoh_data_element(data_element, data_files, api_validation_range=5):
-    with open('./private_files/validation_source_key.json', 'r+') as key_file:
+    with open('./private_test_files/validation_source_key.json', 'r+') as key_file:
         data = json.load(key_file)
-    validation_file = pd.ExcelFile('./private_files/GIS_Measurement_V2_03-04-2022_Validation.xlsx')
+    validation_file = pd.ExcelFile('./private_test_files/GIS_Measurement_V2_03-04-2022_Validation.xlsx')
     true_data_frame = pd.read_excel(validation_file, data[data_element.friendly_name], dtype='str')
     total = 0
     matched = 0
+    empty_cache = GeoenhancedCache()
     if data_element.get_strategy == GetStrategy.FILE:
         for index, row in true_data_frame.iterrows():
             arguments = {"fips_concatenated_code": true_data_frame.iloc[index][constant.GEO_ID_NAME]}
             source_value = true_data_frame.iloc[index]["NUMERIC"]
-            data_value = value_getter.get_value(data_element, arguments, data_files)
+            data_value = value_getter.get_value(data_element, arguments, data_files, empty_cache)
             if data_value is not constant.NOT_AVAILABLE and (not math.isnan(float(source_value))):
                 total += 1
                 if abs(float(source_value) - float(data_value)) < .01:
                     matched += 1
                 # else:
                 #     print(source_value, data_value)
-    elif data_element.get_strategy == GetStrategy.PRIVATE_API:
+    elif data_element.get_strategy in [GetStrategy.PRIVATE_API, GetStrategy.CALCULATION]:
         section = int(len(true_data_frame) / api_validation_range)
         while total < api_validation_range:
             index = random.randint(total * section, (total + 1) * section)
             arguments = {"fips_concatenated_code": true_data_frame.iloc[index][constant.GEO_ID_NAME]}
             source_value = true_data_frame.iloc[index]["NUMERIC"]
-            data_value = value_getter.get_value(data_element, arguments, data_files)
+            print(source_value)
+            data_value = value_getter.get_value(data_element, arguments, data_files, empty_cache)
             if data_value is not constant.NOT_AVAILABLE and (not math.isnan(float(source_value))):
                 total += 1
                 if abs(float(source_value) - float(data_value)) < .01:
@@ -51,13 +54,16 @@ data_elements = sds.SedohDataElements().data_elements
 data_files = main.load_data_files()
 validation_results = []
 for data_element in data_elements:
-    validation_results.append([data_element.friendly_name, validate_sedoh_data_element(data_element, data_files)])
-    
-header = ['variable', 'percent_accurate']
-with open('./private_files/validation_results.csv', 'w', newline='') as result_file:
-    writer = csv.writer(result_file)
-    writer.writerow(header)
-    writer.writerows(validation_results)
+    if data_element.friendly_name in ['Percent Below 200% of Fed Poverty Level', 'Percent Below 300% of Fed Poverty Level']:
+        validation_results.append([data_element.friendly_name, validate_sedoh_data_element(data_element, data_files)])
+
+print(validation_results)
+
+# header = ['variable', 'percent_accurate']
+# with open('./private_files/validation_results.csv', 'w', newline='') as result_file:
+#     writer = csv.writer(result_file)
+#     writer.writerow(header)
+#     writer.writerows(validation_results)
 
 
 
