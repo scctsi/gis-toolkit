071a58de5eda0f00db57b80c0761b2163c5071be
Hakob Abajian <hakob.abajian@med.usc.edu>
2022-03-29 13:49:00 -0700

    debugging test_enhancement.py

--- tests/test_enhancement.py
+++ tests/test_enhancement.py
@@ -44,6 +44,9 @@ def test_enhancement_validity():
     control_data_frame = importer.import_file('./tests/enhancement_control.csv')
     for data_element in data_elements:
         print(data_element.variable_name)
-        assert enhanced_data_frame.iloc[0][data_element.variable_name] == \
-            control_data_frame.iloc[0][data_element.variable_name]
+        print("newly enhanced:  ", enhanced_data_frame.iloc[0][data_element.variable_name])
+        print("control:         ", control_data_frame.iloc[0][data_element.variable_name])
+        # assert enhanced_data_frame.iloc[0][data_element.variable_name] == \
+        #     control_data_frame.iloc[0][data_element.variable_name]
+        assert 1<0
     # shutil.rmtree('./temp', ignore_errors=False, onerror=handle_remove_readonly)
