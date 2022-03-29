d8bf187eb129708700977391921baf7bdd48fa01
Hakob Abajian <hakob.abajian@med.usc.edu>
2022-03-29 09:39:23 -0700

    fixed enhancement_control.csv location

--- tests/test_enhancement.py
+++ tests/test_enhancement.py
@@ -42,5 +42,5 @@ def test_enhancement_validity():
     sedoh_enhancer = DataFrameEnhancer(input_data_frame, data_elements, data_files, data_key, True)
     enhanced_data_frame = sedoh_enhancer.enhance().iloc[[2]]
     enhanced_data_frame.index = [0]
-    assert enhanced_data_frame.equals(importer.import_file('./enhancement_control.csv'))
+    assert enhanced_data_frame.equals(importer.import_file('./tests/enhancement_control.csv'))
     shutil.rmtree('./temp', ignore_errors=False, onerror=handle_remove_readonly)
