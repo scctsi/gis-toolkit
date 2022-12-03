ec8afa0a8d6ab38363118a086e86bb17f7673264
Hakob Abajian <hakob.abajian@med.usc.edu>
2022-12-02 19:17:39 -0800

    added data_files construction algorithm and json

--- data_files.py
+++ data_files.py
@@ -1,6 +1,9 @@
 import os
-from data_structure import DataSource, RasterSource
+from datetime import datetime
+
+from data_structure import ACSSource, DataSource, RasterSource, NasaSource
 from sedoh_data_structure import SedohDataSource
+import json
 
 
 class DataFiles:
@@ -26,3 +29,61 @@ class DataFiles:
                         self.data_files[data_source].append(
                             RasterSource()
                         )
+
+
+class DataFiles:
+    def __init__(self):
+        with open('temp/geocoder_save_file.json') as save_file:
+            data = json.load(save_file)
+        self.data_files = {}
+        for source in data:
+            if data[source]["type"] == "ACS":
+                data_sources = []
+                for version in data[source]["versions"]:
+                    data_sources.append(ACSSource(
+                        acs_year=version["acs_year"],
+                        start_date=datetime.strptime(version["valid_start_date"], "%Y-%m-%dT%H:%M:%S.%fZ"),
+                        end_date=datetime.strptime(version["valid_end_date"], "%Y-%m-%dT%H:%M:%S.%fZ")
+                    ))
+                self.data_files.update({vars(SedohDataSource)["_member_map_"][source]: data_sources})
+            elif data[source]["type"] == "Census":
+                data_sources = []
+                for version in data[source]["versions"]:
+                    data_sources.append(DataSource(
+                        file_name=version["file_path"],
+                        tract_column=version["census_tract_column_name"],
+                        start_date=datetime.strptime(version["valid_start_date"], "%Y-%m-%dT%H:%M:%S.%fZ"),
+                        end_date=datetime.strptime(version["valid_end_date"], "%Y-%m-%dT%H:%M:%S.%fZ")
+                    ))
+                self.data_files.update({vars(SedohDataSource)["_member_map_"][source]: data_sources})
+            elif data[source]["type"] == "Raster":
+                pollutants = [key for key in data[source] if key != "type"]
+                for pollutant in pollutants:
+                    data_sources = []
+                    for version in data[source][pollutant]["versions"]:
+                        data_sources.append(RasterSource(
+                            file_name=version["file_path"],
+                            latitude_range=version["latitude_range"],
+                            longitude_range=version["longitude_range"],
+                            precision=version["precision"],
+                            start_date=datetime.strptime(version["valid_start_date"], "%Y-%m-%dT%H:%M:%S.%fZ"),
+                            end_date=datetime.strptime(version["valid_end_date"], "%Y-%m-%dT%H:%M:%S.%fZ")
+                        ))
+                    self.data_files.update({(vars(SedohDataSource)["_member_map_"][source], pollutant): data_sources})
+            elif data[source]["type"] == "Nasa":
+                pollutants = [key for key in data[source] if key != "type"]
+                for pollutant in pollutants:
+                    data_sources = []
+                    for version in data[source][pollutant]["versions"]:
+                        data_sources.append(NasaSource(
+                            file_name=version["file_path"],
+                            start_date=datetime.strptime(version["valid_start_date"], "%Y-%m-%dT%H:%M:%S.%fZ"),
+                            end_date=datetime.strptime(version["valid_end_date"], "%Y-%m-%dT%H:%M:%S.%fZ")
+                        ))
+                    self.data_files.update({(vars(SedohDataSource)["_member_map_"][source], pollutant): data_sources})
+        print("Data Files Load Complete")
+
+
+
+
+
